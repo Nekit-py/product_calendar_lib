5b7b04ca6afcbf39e8d13a6c1dc63417e4f1a303
sidorovich_ns <61461523+Nekit-py@users.noreply.github.com>
2024-05-15 17:30:08 +0700

    попытка создания pyclass

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,55 @@ pub mod statistic;
 
 use pyo3::prelude::*;
 use std::collections::HashMap;
+use day::Day as RustDay;
 
-#[pyfunction]
-fn prod_cal(year: Option<u16>) -> PyResult<Vec<HashMap<String, String>>> {
-    let calendar = pc::get_product_calendar(year).unwrap();
-    Ok(calendar.as_vec_hashmap())
+
+#[pyclass]
+pub struct Day(RustDay);
+
+#[pymethods]
+impl Day {
+    #[getter]
+    fn weekday(&self) -> String {
+        self.0.weekday.to_string()
+    }
+
+    #[getter]
+    fn day(&self) -> String {
+        format!("{}", self.0.day.format("%Y-%m-%d"))
+    }
+
+    #[getter]
+    fn kind(&self) -> String {
+        self.0.kind.to_string()
+    }
 }
 
-#[pyfunction]
-fn prod_cal_statistic(year: Option<u16>) -> PyResult<HashMap<String, u16>> {
-    let calendar = pc::get_product_calendar(year).unwrap();
-    Ok(calendar.statistic().as_map())
-}
 
 #[pymodule]
 fn product_calendar(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(prod_cal, m)?)?;
-    m.add_function(wrap_pyfunction!(prod_cal_statistic, m)?)?;
+    // m.add_function(wrap_pyfunction!(prod_cal, m)?)?;
+    // m.add_function(wrap_pyfunction!(prod_cal_statistic, m)?)?;
+    m.add_class::<Day>()?;
     Ok(())
 }
+
+
+// #[pyfunction]
+// fn prod_cal(year: Option<u16>) -> PyResult<Vec<HashMap<String, String>>> {
+//     let calendar = pc::get_product_calendar(year).unwrap();
+//     Ok(calendar.as_vec_hashmap())
+// }
+
+// #[pyfunction]
+// fn prod_cal_statistic(year: Option<u16>) -> PyResult<HashMap<String, u16>> {
+//     let calendar = pc::get_product_calendar(year).unwrap();
+//     Ok(calendar.statistic().as_map())
+// }
+
+// #[pymodule]
+// fn product_calendar(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+//     m.add_function(wrap_pyfunction!(prod_cal, m)?)?;
+//     m.add_function(wrap_pyfunction!(prod_cal_statistic, m)?)?;
+//     Ok(())
+// }
