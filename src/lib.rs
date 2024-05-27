cf9524e76c3b986149a48b077bc4192aeda2e439
Nekit-py <nekit-sns@yandex.ru>
2024-05-27 19:59:39 +0700

    добавил докстринги

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,18 @@ pub struct ProductCalendar(RustProductCalendar);
 #[pymethods]
 impl ProductCalendar {
     #[new]
+    /// Создает новый экземпляр ProductCalendar.
+    ///
+    /// # Аргументы
+    /// * `year` - Опциональный год для календаря.
+    ///
+    /// # Пример
+    /// ```
+    /// from product_calendar import ProductCalendar
+    ///
+    ///
+    /// calendar = ProductCalendar(2024)
+    /// ```
     #[pyo3(signature=(year=None))]
     fn new(year: Option<u16>) -> PyResult<Self> {
         match get_product_calendar(year) {
@@ -29,6 +41,16 @@ impl ProductCalendar {
         }
     }
 
+    /// Возвращает день после указанного количества недель от заданной даты.
+    ///
+    /// # Аргументы
+    /// * `date` - Начальная дата.
+    /// * `weeks` - Количество недель.
+    /// # Пример
+    /// ```
+    /// let calendar = ProductCalendar::new(Some(2024));
+    /// let desired_day = calendar.after_nth_weeks()
+    /// ```
     fn after_nth_weeks(&self, date: NaiveDate, weeks: usize) -> PyResult<Day> {
         match self.0.after_nth_weeks(date, weeks) {
             Ok(d) => Ok(Day(d)),
@@ -36,6 +58,11 @@ impl ProductCalendar {
         }
     }
 
+    /// Возвращает календарь за указанный период начиная с даты и длиной в количество дней.
+    ///
+    /// # Аргументы
+    /// * `date` - Начальная дата.
+    /// * `days` - Количество дней.
     fn period_by_number_of_days(&self, date: NaiveDate, days: usize) -> PyResult<Self> {
         match self.0.period_by_number_of_days(date, days) {
             Ok(rpc) => Ok(Self(rpc)),
@@ -43,6 +70,11 @@ impl ProductCalendar {
         }
     }
 
+    /// Возвращает календарь за указанный период начиная с даты и длиной в количество рабочих дней.
+    ///
+    /// # Аргументы
+    /// * `date` - Начальная дата.
+    /// * `work_days` - Количество рабочих дней.
     fn period_by_number_of_work_days(&self, date: NaiveDate, work_days: usize) -> PyResult<Self> {
         match self.0.period_by_number_of_work_days(date, work_days) {
             Ok(rpc) => Ok(Self(rpc)),
@@ -50,6 +82,11 @@ impl ProductCalendar {
         }
     }
 
+    /// Возвращает календарь за указанный период между двумя датами.
+    ///
+    /// # Аргументы
+    /// * `start` - Начальная дата.
+    /// * `end` - Конечная дата.
     fn period_slice(&self, start: NaiveDate, end: NaiveDate) -> PyResult<Self> {
         match self.0.period_slice(start, end) {
             Ok(rpc) => Ok(Self(rpc)),
@@ -57,6 +94,10 @@ impl ProductCalendar {
         }
     }
 
+    /// Возвращает даты за указанный квартал.
+    ///
+    /// # Аргументы
+    /// * `quarter` - Номер квартала (1, 2, 3 или 4).
     fn extract_dates_in_quarter(&self, quarter: u8) -> PyResult<Self> {
         match self.0.extract_dates_in_quarter(quarter) {
             Ok(calendar) => Ok(Self(calendar)),
@@ -64,14 +105,20 @@ impl ProductCalendar {
         }
     }
 
+    /// Возвращает статистику по календарю.
     fn statistic(&self) -> PyResult<Statistic> {
         Ok(Statistic(self.0.statistic()))
     }
 
+    /// Возвращает общее количество дней в календаре.
     fn total_days(&self) -> PyResult<usize> {
         Ok(self.0.total_days())
     }
 
+    /// Возвращает следующий рабочий день после указанной даты.
+    ///
+    /// # Аргументы
+    /// * `cur_day` - Текущая дата.
     fn next_work_day(&self, cur_day: NaiveDate) -> PyResult<Day> {
         match self.0.next_work_day(cur_day) {
             Ok(d) => Ok(Day(d)),
@@ -79,11 +126,16 @@ impl ProductCalendar {
         }
     }
 
+    /// Возвращает календарь, отфильтрованный по типу дня.
+    ///
+    /// # Аргументы
+    /// * `kind` - Тип дня (например, "Work", "Weekend").
     fn by_kind(&self, kind: &str) -> PyResult<Self> {
         let kind = DayKind::from_str(kind).unwrap();
         Ok(Self(self.0.by_kind(kind)))
     }
 
+    /// Возвращает все дни в календаре.
     fn all_days(&self) -> PyResult<Vec<Day>> {
         Ok(self.0.calendar.iter().map(|d| Day(d.clone())).collect())
     }
@@ -94,6 +146,13 @@ pub struct Statistic(RustStatistic);
 
 #[pymethods]
 impl Statistic {
+    /// Создает новый экземпляр Statistic.
+    ///
+    /// # Аргументы
+    /// * `holidays` - Количество праздничных дней.
+    /// * `work_days` - Количество рабочих дней.
+    /// * `weekends` - Количество выходных дней.
+    /// * `preholidays` - Количество предпраздничных дней.
     #[new]
     #[pyo3(signature=(holidays=0, work_days=0, weekends=0, preholidays=0))]
     fn new(holidays: u16, work_days: u16, weekends: u16, preholidays: u16) -> Self {
@@ -105,14 +164,17 @@ impl Statistic {
         })
     }
 
+    /// Возвращает количество рабочих часов.
     fn work_hours(&self) -> PyResult<u16> {
         Ok(self.0.work_hours())
     }
 
+    /// Возвращает количество дней отдыха.
     fn rest_days(&self) -> PyResult<u16> {
         Ok(self.0.rest_days())
     }
 
+    /// Возвращает статистику в виде словаря.
     fn as_dict<'py>(&self, py: Python<'py>) -> Bound<'py, PyDict> {
         self.0.as_map().into_py_dict_bound(py)
     }
@@ -169,15 +231,13 @@ impl Statistic {
 #[pyclass]
 pub struct Day(RustDay);
 
-//TODO Переработать RustDay таким образом, чтобы принмал только дату,
-// остальные поля сам рассчитывал
 #[pymethods]
 impl Day {
-    /// new(day, /)
-    /// --
+    /// Создает новый экземпляр Day.
+    /// Принимает опционально объект datetime.date.
     ///
-    /// Эта функция инициализирует класс Day.
-    /// Принимает опционально объект datetime.date
+    /// # Аргументы
+    /// * `day` - Опциональная дата.
     #[new]
     #[pyo3(signature=(day=None))]
     fn new(day: Option<NaiveDate>) -> Self {
