3c500eba51e97ee5ae0f92713a338fa63c35551a
Nekit-py <nekit-sns@yandex.ru>
2024-06-19 20:16:36 +0700

    убрал лишнее с ридмишки, чуток упроситл тест у парсера

--- src/parser.rs
+++ src/parser.rs
@@ -123,16 +123,16 @@ mod tests {
     #[test]
     fn test_parse_calendar() {
         let mut parser = super::ProductCalendarParser::new(2024);
-        match parser.parse_calendar() {
-            Ok(cal) => {
-                let d1 = super::Day {
-                    weekday: chrono::Weekday::Mon,
-                    day: chrono::NaiveDate::from_ymd_opt(2024, 1, 1).unwrap(),
-                    kind: super::DayKind::Holiday,
-                };
-                assert_eq!(cal[0], d1);
-            }
-            Err(_) => (),
-        }
+        let parsed_calendar = parser
+            .parse_calendar()
+            .expect("Не удалось распарсить календарь");
+
+        let expected_day = super::Day {
+            weekday: chrono::Weekday::Mon,
+            day: chrono::NaiveDate::from_ymd_opt(2024, 1, 1).unwrap(),
+            kind: super::DayKind::Holiday,
+        };
+
+        assert_eq!(parsed_calendar[0], expected_day);
     }
 }
